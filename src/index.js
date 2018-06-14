dd5bc54164564923c4f8a64b1b1f76600a2ccd88
asautour <asautour@users.noreply.github.com>
2018-06-14 16:34:35 +0100

    basic stuff

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "babel-core";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import allReducers from "./js/reducers";
+import App from "./js/components/app";
+
+import "./styles.css";
+
+const store = createStore(allReducers);
+
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
